9b8b93d6f1b65073fe8eb19ac4b63d4bb8bb9f43
Robert Winterstein <rrwinterstein@gmail.com>
2019-06-03 18:29:14 -0700

    Adds Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,13 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-125052245-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
