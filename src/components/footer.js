2bd8d77b89be6f49cb121da4882c61f89981df62
Robert Winterstein <rrwinterstein@gmail.com>
2020-05-04 17:01:55 -0400

    Moves social links to separate component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-
-// Fontawesome icons
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { fab } from "@fortawesome/free-brands-svg-icons"
-library.add(fab)
+import SocialLinks from "../components/socialLinks"
 
 const Footer = styled.footer`
   background: var(--white);
@@ -28,22 +23,6 @@ const Footer = styled.footer`
     flex-direction: column;
   }
 
-  .social {
-    display: flex;
-    align-items: center;
-    font-size: 1.5rem;
-    margin-bottom: 16px;
-
-    a {
-      color: var(--gray);
-      margin-right: 16px;
-    }
-  }
-
-  FontAwesomeIcon {
-    display: flex;
-  }
-
   .copyright {
     font-family: "Nunito";
     font-style: normal;
@@ -64,36 +43,6 @@ const Footer = styled.footer`
       margin-bottom: 40px;
     }
 
-    .social {
-      margin-bottom: 24px;
-      margin-left: -10px;
-      font-size: 1.75rem;
-
-      a {
-        height: 36px;
-        width: 36px;
-        display: flex;
-        justify-content: center;
-        box-sizing: content-box;
-        border-radius: 10px;
-        padding: 8px 10px;
-        margin-right: 8px;
-        -webkit-transition: all 200ms ease-in-out;
-        -mox-transition: all 200ms ease-in-out;
-        -o-transition: all 200ms ease-in-out;
-        transition: all 200ms ease-in-out;
-
-        :hover {
-          color: var(--grayDark);
-          background-color: var(--grayLight);
-          -webkit-transition: all 200ms ease-in-out;
-          -mox-transition: all 200ms ease-in-out;
-          -o-transition: all 200ms ease-in-out;
-          transition: all 200ms ease-in-out;
-        }
-      }
-    }
-
     .copyright {
       font-size: 12px;
       line-height: 16px;
@@ -105,14 +54,6 @@ const Footer = styled.footer`
 
     .contact {
       margin-bottom: 180px;
-    }
-
-    .social {
-      font-size: 2rem;
-
-      a {
-        margin-right: 16px;
-      }
     }
 
     .copyright {
@@ -164,45 +105,8 @@ const footer = () => {
         <h2>Get in touch</h2>
 
         <div className="methods">
-          <div className="social">
-            <a
-              href="https://dribbble.com/rwinterstein"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Dribbble"
-            >
-              <FontAwesomeIcon icon={["fab", "dribbble"]} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/wintersteinrobert/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="LinkedIn"
-            >
-              <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
-            </a>
-            <a
-              href="https://www.instagram.com/robwinterstein/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Instagram"
-            >
-              <FontAwesomeIcon icon={["fab", "instagram"]} />
-            </a>
-            <a
-              href="https://github.com/rwinterstein"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="GitHub"
-            >
-              <FontAwesomeIcon icon={["fab", "github-alt"]} />
-            </a>
-          </div>
-
-          <Button
-            className="btn btn-primary"
-            href="mailto:hello@rwinterstein.com?subject=Hello!"
-          >
+          <SocialLinks />
+          <Button href="mailto:hello@rwinterstein.com?subject=Hello!">
             Email
           </Button>
         </div>
---
+++ src/components/socialLinks.js
@@ -0,0 +1,102 @@
+import React from "react"
+import styled from "styled-components"
+
+// Fontawesome icons
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+library.add(fab)
+
+const SocialLinks = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.75rem;
+  margin-bottom: 16px;
+
+  a {
+    color: var(--gray);
+    margin-right: 16px;
+  }
+
+  FontAwesomeIcon {
+    display: flex;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 24px;
+    margin-left: -10px;
+
+    a {
+      height: fit-content;
+      width: fit-content;
+      display: flex;
+      justify-content: center;
+      box-sizing: content-box;
+      border-radius: 10px;
+      padding: 8px 10px;
+      margin-right: 8px;
+      -webkit-transition: all 200ms ease-in-out;
+      -mox-transition: all 200ms ease-in-out;
+      -o-transition: all 200ms ease-in-out;
+      transition: all 200ms ease-in-out;
+
+      :hover {
+        color: var(--grayDark);
+        background-color: var(--grayLight);
+        -webkit-transition: all 200ms ease-in-out;
+        -mox-transition: all 200ms ease-in-out;
+        -o-transition: all 200ms ease-in-out;
+        transition: all 200ms ease-in-out;
+      }
+    }
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 2rem;
+
+    a {
+      margin-right: 16px;
+    }
+  }
+`
+
+const socialLinks = () => {
+  return (
+    <SocialLinks>
+      <a
+        href="https://dribbble.com/rwinterstein"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Dribbble"
+      >
+        <FontAwesomeIcon icon={["fab", "dribbble"]} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/wintersteinrobert/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="LinkedIn"
+      >
+        <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+      </a>
+      <a
+        href="https://www.instagram.com/robwinterstein/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Instagram"
+      >
+        <FontAwesomeIcon icon={["fab", "instagram"]} />
+      </a>
+      <a
+        href="https://github.com/rwinterstein"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="GitHub"
+      >
+        <FontAwesomeIcon icon={["fab", "github-alt"]} />
+      </a>
+    </SocialLinks>
+  )
+}
+
+export default socialLinks
